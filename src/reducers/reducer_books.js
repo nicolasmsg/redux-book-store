24a6e4b531568489d01a2758553cfecb80a0be35
nicolasmsg <nicolas.msg@protonmail.com>
2017-09-13 11:07:34 +0700

    its now possible to select a book and get detail

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    {title: 'Book 1', pages: 101},
+    {title: 'Book 2', pages:300},
+    {title: 'Book 3', pages:342},
+    {title: 'Book 4', pages:434}
+  ]
+}
